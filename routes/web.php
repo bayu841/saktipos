be908af804bf05f4aca8d133a669605b7e1b10c4
Bayu Ardiansyah <bayuardiansyah206@gmail.com>
2025-12-19 09:28:20 +0700

    ganti route

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('solusibisnis');
 });
-=======
-    return view('landingpage');
-});
->>>>>>> ba0b3809685ad58b12d25514af14c0964cf1f210
